6882c73136c2314a9dfc0c1b3a0efd9891032100
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-12-28 13:04:48 +0400

    fix: file uploading headers

--- frontend/src/hooks/posts/useCreatePost.ts
+++ frontend/src/hooks/posts/useCreatePost.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { parseErrorMessage } from "../../utils/parseError";
-import { api } from "../../utils/axios";
 import type { Post } from "./useGetFeed";
+import { api } from "../../utils/axios";
 
 export const useCreatePost = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -19,7 +19,12 @@ export const useCreatePost = () => {
         });
 
         try {
-            const response = await api.post(`/posts`, formData);
+            const response = await api.post(`/posts`, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                withCredentials: true,
+            });
 
             return response.data.data.post as Post;
         } catch (err: any) {
